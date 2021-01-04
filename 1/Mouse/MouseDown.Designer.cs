39f78d445ace764b917f2ff07d584488ceac26b2
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-01-04 18:16:57 +0900

    그림판

---
+++ 1/Mouse/MouseDown.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace _1.Mouse
+{
+    partial class MouseDown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.save = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.color = new System.Windows.Forms.Button();
+            this.colorpanel = new System.Windows.Forms.Panel();
+            this.remove = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.getImg = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(963, 12);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(148, 124);
+            this.save.TabIndex = 1;
+            this.save.Text = "SAVE";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.BackgroundImageChanged += new System.EventHandler(this.getImg_Click);
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 143);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1099, 357);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // color
+            // 
+            this.color.Location = new System.Drawing.Point(81, 82);
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(123, 54);
+            this.color.TabIndex = 2;
+            this.color.Text = "color";
+            this.color.UseVisualStyleBackColor = true;
+            this.color.Click += new System.EventHandler(this.color_Click);
+            // 
+            // colorpanel
+            // 
+            this.colorpanel.Location = new System.Drawing.Point(12, 82);
+            this.colorpanel.Name = "colorpanel";
+            this.colorpanel.Size = new System.Drawing.Size(63, 52);
+            this.colorpanel.TabIndex = 3;
+            // 
+            // remove
+            // 
+            this.remove.Location = new System.Drawing.Point(12, 12);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(192, 64);
+            this.remove.TabIndex = 4;
+            this.remove.Text = "remove";
+            this.remove.UseVisualStyleBackColor = true;
+            this.remove.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(210, 12);
+            this.trackBar1.Maximum = 15;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // getImg
+            // 
+            this.getImg.Location = new System.Drawing.Point(811, 12);
+            this.getImg.Name = "getImg";
+            this.getImg.Size = new System.Drawing.Size(146, 125);
+            this.getImg.TabIndex = 6;
+            this.getImg.Text = "get IMG";
+            this.getImg.UseVisualStyleBackColor = true;
+            this.getImg.Click += new System.EventHandler(this.getImg_Click);
+            // 
+            // MouseDown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 512);
+            this.Controls.Add(this.getImg);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.remove);
+            this.Controls.Add(this.colorpanel);
+            this.Controls.Add(this.color);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.save);
+            this.Name = "MouseDown";
+            this.Text = "MouseDown1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Closing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button color;
+        private System.Windows.Forms.Panel colorpanel;
+        private System.Windows.Forms.Button remove;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button getImg;
+    }
+}
---
+++ 1/Mouse/MouseDown.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1.Mouse
+{
+    public partial class MouseDown : Form
+    {
+        int curLineSize = 5;
+        Color curColor = Color.Black;
+        Bitmap pictureBoxBmp;
+        Point mouseDownPoint;
+        int saveCount = Properties.Settings.Default.saveCount;
+
+        enum SELECTED_MODE : int
+        {
+            LINE = 0,
+            SQUARE = 1,
+        }
+
+        public MouseDown()
+        {
+            InitializeComponent();
+            pictureBoxBmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            colorpanel.BackColor = curColor;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                Point curPoint = pictureBox1.PointToClient(new Point(Control.MousePosition.X, Control.MousePosition.Y));
+
+                Pen p;
+                p = new Pen(curColor);
+
+                p.Width = curLineSize;
+
+                Graphics g = Graphics.FromImage(pictureBoxBmp);
+                g.DrawEllipse(p, curPoint.X, curPoint.Y, p.Width, p.Width);
+
+                p.Dispose();
+                g.Dispose();
+            }
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDownPoint = new Point(e.X, e.Y);
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                Point startPoint = new Point(e.X, e.Y);
+                Color preColor = pictureBoxBmp.GetPixel(startPoint.X, startPoint.Y);
+                pictureBox1.Image = pictureBoxBmp;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Pen p = new Pen(curColor);
+                p.Width = curLineSize;
+
+                Point mouseUpPoint = new Point(e.X, e.Y);
+
+                Graphics g = Graphics.FromImage(pictureBoxBmp);
+                g.DrawRectangle(p, new Rectangle(mouseDownPoint.X, mouseDownPoint.Y, Math.Abs(mouseUpPoint.X - mouseDownPoint.X), Math.Abs(mouseUpPoint.Y - mouseDownPoint.Y)));
+                pictureBox1.Image = pictureBoxBmp;
+
+                p.Dispose();
+                g.Dispose();
+            }
+        }
+
+        public void save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = ($"img{saveCount}.png");
+            saveFileDialog.Filter = "PNG File|*.png|Bitmap File|*.bmp|JPEG File|*.jpg";
+            if (pictureBox1.Image == null)
+            {
+
+                return;
+            }
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = (FileStream)saveFileDialog.OpenFile();
+                switch (saveFileDialog.FilterIndex)
+                {
+                    case 1:
+                        this.pictureBox1.Image.Save(fs, ImageFormat.Png);
+                        break;
+                    case 2:
+                        this.pictureBox1.Image.Save(fs, ImageFormat.Bmp);
+                        break;
+                    case 3:
+                        this.pictureBox1.Image.Save(fs, ImageFormat.Jpeg);
+                        break;
+                }
+                fs.Dispose();
+                fs.Close();
+            }
+            Properties.Settings.Default.saveCount++;
+            Properties.Settings.Default.Save();
+            Properties.Settings.Default.Reload();
+        }
+
+        private void color_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+            if(colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                curColor = colorDialog.Color;
+                colorpanel.BackColor = curColor;
+            }
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            if(curColor != Color.White)
+            {
+                curColor = Color.White;
+                colorpanel.BackColor = Color.White;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            curLineSize = trackBar1.Value;
+        }
+
+        private void getImg_Click(object sender, EventArgs e)
+        {
+            string image_file = string.Empty;
+
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image_file = dialog.FileName;
+            }
+            else if (dialog.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.BackgroundImage = System.Drawing.Image.FromFile(image_file);
+        }
+
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                if(MessageBox.Show("저장하지 않고 종료하시겠습니까?", "네", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    save_Click(sender, e);
+                    Properties.Settings.Default.Save();
+                    Properties.Settings.Default.Reload();
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
